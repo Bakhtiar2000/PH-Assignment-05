a02f0ed025b94a0b16f4d18d3fda23cf9cc62aff
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-02-18 23:52:43 +0600

    Random color updated

--- js/randomColor.js
+++ js/randomColor.js
@@ -1,23 +1,10 @@
-// function randomColor() {
-//     let color = [];
-//     for (let i = 0; i < 3; i++) {
-//       color.push(Math.floor(Math.random() * 256));
-//     }
-//     return 'rgb(' + color.join(', ') + ')';
-//   } 
-
-// document.getElementById('triangle-container').addEventListener('mouseover',function(event){
-//     event.target.style.background= randomColor();
-// })
-
 
 var menuBoxes = document.getElementsByClassName('MenuBox');
 for (let i = 0; i < menuBoxes.length; i++) {
     menuBoxes[i].onmouseover = function(e) {
         let color = '#'+Math.floor(Math.random()*16777215).toString(16);
-        let colorString = '0px 0px 30px 0px ' + color;
-        this.style['box-shadow'] = colorString;
-        this.style['-webkit-box-shadow'] = colorString;
-        this.style['-moz-box-shadow'] = colorString;
+        this.style['background-color'] = color;
+        this.style['-webkit-background-color'] = color;
+        this.style['-moz-background-color'] = color;
     }  
 }
